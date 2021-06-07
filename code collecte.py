fd950680c3e05d6bee494c7dce42b7ed939d421e
zhaozhichao <zhaozhichao.ml@gmail.com>
2021-06-07 15:49:02 +0800

    Create code collecte.py

---
+++ code collecte.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# 对 bbox 进行顺时针排序
+def order_points_clockwise(pts):
+    rect = np.zeros((4, 2), dtype="float32")
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    return rect
+
+# 对一系列的点进行顺时针排序
+def order_points_clockwise_list(pts):
+    pts = pts.tolist()
+    pts.sort(key=lambda x: (x[1], x[0]))
+    pts[:2] = sorted(pts[:2], key=lambda x: x[0])
+    pts[2:] = sorted(pts[2:], key=lambda x: -x[0])
+    pts = np.array(pts)
+    return pts
